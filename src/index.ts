58130d3f84df4cf199ec754a37e36a79185a8de9
m1sk9 <me@m1sk9.dev>
2024-05-14 16:13:44 +0900

    feat: add `/docs`, `/api` endpoint

--- src/index.ts
+++ src/index.ts
@@ -6,6 +6,14 @@ app.get("/", (c) => {
 	return c.redirect("https://pulsate.dev", 302);
 });
 
+app.get("/docs", (c) => {
+	return c.redirect("https://docs.pulsate.dev", 302);
+});
+
+app.get("/api", (c) => {
+	return c.redirect("https://api.pulsate.dev/reference", 302);
+});
+
 app.get("/discord", (c) => {
 	return c.redirect("https://discord.gg/NmczMnWGvh", 302);
 });
@@ -22,12 +30,12 @@ app.get("/youtube", (c) => {
 	return c.redirect("https://youtube.com/@pulsate-dev", 302);
 });
 
-app.get("/discussions", (c) => {
-	return c.redirect("https://github.com/orgs/pulsate-dev/discussions", 302);
-});
-
 app.get("/bluesky", (c) => {
 	return c.redirect("https://bsky.app/profile/pulsate.dev", 302);
+});
+
+app.get("/discussions", (c) => {
+	return c.redirect("https://github.com/orgs/pulsate-dev/discussions", 302);
 });
 
 export default app;
